076cd72bd7d5c78bd5faba15a006efd0b90d8557
vlpl5634AS <vlpl5634@leeds.ac.uk>
2026-02-12 20:10:19 +0000

    portfolio for 1850 completed

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -4,20 +4,28 @@
 /*
  * Potrfolio submission
  * Name: Ahmed Sharkasi
- * ID:
+ * ID: 201826539
  */
-
+ #include <stdio.h>
+ #include <math.h>
  int main( void ) {
 
     // define and initialise variables for the problem data 
+   float national_insurance = 0.08;
+   float tax_rate = 0.15;
+   float income = 36250.00;
+   float NI_contribution;
+   float tax_contribution;
+   // calculate the deductions and final take-home salary
 
-    // calculate the deductions and final take-home salary
+   NI_contribution = national_insurance * income;
+   tax_contribution = ((income - NI_contribution) - 12500) * tax_rate;
 
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+    printf("Salary £%.2f\n", income);
+    printf("NI contribution £%.2f\n", NI_contribution);
+    printf("Tax contribution £%.2f\n", tax_contribution);
+    printf("Take home salary £%.2f\n", income - NI_contribution - tax_contribution);
 
     return 0;
  }
