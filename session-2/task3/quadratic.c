44f7cba288182e692d4f17b0802aa8beadd8ca08
vlpl5634AS <vlpl5634@leeds.ac.uk>
2026-02-12 15:51:51 +0000

    first task of s2 t3 finished

--- session-2/task3/quadratic.c
+++ session-2/task3/quadratic.c
@@ -15,13 +15,17 @@ int main( void ) {
     float b = -5.0;
     float c = 6.0;
     float root1, root2;
-
+    float discriminant;
     /*
      * Implement the formula for the 2 roots of a quadratic.
      * You can define additional variable for intermediate results to make the code simpler.
      * Print out the final results for the 2 roots as float values.
      */
 
+     discriminant = b * b - 4 * a * c;
+     root1 = (-b + sqrt(discriminant)) / (2 * a);
+     root2 = (-b - sqrt(discriminant)) / (2 * a);
+     printf("root1 = %f and root2 = %f", root1, root2);
     
 
     return 0;
